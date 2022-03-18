bb771fe3712538c39e8f5b4277283393d4275513
Alex Contra <contraalex8@gmail.com>
2022-03-18 20:42:21 +0200

    integrated project base structure

---
+++ expertsystems/lib/home/hello/hello_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HelloPage extends StatefulWidget {
+  const HelloPage({Key? key}) : super(key: key);
+
+  @override
+  State<HelloPage> createState() => _HelloPageState();
+}
+
+class _HelloPageState extends State<HelloPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
